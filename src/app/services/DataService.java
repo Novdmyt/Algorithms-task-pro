051ff91ce4ff159f2b881bd15cdba259133c99c9
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-12 20:08:56 +0300

    Rename method and fix

--- src/app/services/DataService.java
+++ src/app/services/DataService.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class DataService {
     // Алгоритм лінійного пошуку
-    public int search(List<Product> list, double x) {
-        for (int i = 0; i < list.; i++) {
-            if (list.get(i) == x) return i;
+    public int searchPrice(List<Product> list, double x) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getPrice() == x) return i;
         }
         return -1;
     }
